314818921d0a8bf2cd4d8224d955f4c5cba8235e
kaul <ankurkaul1894@gmail.com>
2020-04-18 22:46:45 -0700

    react setup with basic 5 components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
+
+const Header = (props) => (
+    <header>
+        <nav>
+            <div></div>
+            <div className="header__logo"></div>
+            <div></div>
+        </nav>
+    </header>
+)
+
+export default Header
