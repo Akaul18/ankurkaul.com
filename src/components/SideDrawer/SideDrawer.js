90b87687bc1d97f517a68090555b2b6c42fb38e7
kaul <ankurkaul1894@gmail.com>
2020-04-21 21:25:07 -0700

    side drawer closing success on li click

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -2,25 +2,26 @@ import React from 'react'
 import './SideDrawer.scss'
 import { Link } from 'react-router-dom'
 
-const SideDrawer = props => {
+const SideDrawer = ({ showDrawer, closeDrawer }) => {
     let drawerClasses = 'side-drawer';
 
-    if (props.showDrawer) {
+    if (showDrawer) {
         drawerClasses = 'side-drawer open-drawer'
     }
+
     return (
         <nav className={drawerClasses}>
-            <div className="cross-button" onClick={props.closeDrawer}>
+            <div className="cross-button" onClick={closeDrawer}>
                 <div className="cross-button__line"></div>
                 <div className="cross-button__line"></div>
                 <div className="cross-button__line"></div>
             </div>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About Me</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/skills">Skills</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li onClick={closeDrawer}><Link to="/">Home</Link></li>
+                <li onClick={closeDrawer}><Link to="/about">About Me</Link></li>
+                <li onClick={closeDrawer}><Link to="/projects">Projects</Link></li>
+                <li onClick={closeDrawer}><Link to="/skills">Skills</Link></li>
+                <li onClick={closeDrawer}><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
