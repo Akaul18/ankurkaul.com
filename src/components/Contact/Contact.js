343dce4d9f36a6087b6d93bb3f6cb4e1eddc8b29
kaul <ankurkaul1894@gmail.com>
2020-04-23 23:00:22 -0700

    contact card done excpet links

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import './Contact.scss'
-// import Header from '../Header'
+import { AiOutlineMail } from 'react-icons/ai'
+import { RiLinkedinBoxLine } from 'react-icons/ri'
+import { GoMarkGithub } from 'react-icons/go'
 
-function Contact() {
+const Contact = props => {
     return (
         <div>
-            {/* <Header /> */}
             <main className="contact__main">
-                <div>
-                    <h1>Contact Component</h1>
+                <div className="my-contact-card">
+                    <div className="clipart"></div>
+                    <div>
+                        <h2>ANKUR <span>KAUL</span></h2>
+                        <h3><span>Full -</span> stack developer</h3>
+                        <ul>
+                            <li><span><AiOutlineMail /></span> <span>ankurkaul1894@gmail.com</span></li>
+                            <li><span><RiLinkedinBoxLine /></span> www.linkedin.com/in/ankurkaul1894</li>
+                            <li><span><GoMarkGithub /></span> www.github.com/Akaul18</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="contact-details">
+
                 </div>
             </main>
         </div>
