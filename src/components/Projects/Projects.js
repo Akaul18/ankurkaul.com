dc63373d7ffd2d2a6586c5695c9c277c75a361b6
kaul <ankurkaul1894@gmail.com>
2020-04-26 22:11:54 -0700

    project page mini header and project image div setup

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,39 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import './Projects.scss'
+import instock from '../../assets/images/instock.png';
+import instockMobile from '../../assets/images/instock-mobile.png';
 
 function Project() {
-    const [liState, setLiState] = useState({
-        "All": '',
-        'React.js': '',
-        'Node.js': '',
-        "Php": '',
-    })
 
-    // useEffect(() => {
+    const [width, setWidth] = useState(0)
+    useEffect(() => {
+        window.addEventListener('resize', handleWidth)
+        return () => {
+            window.removeEventListener('resize', handleWidth)
+        }
+    }, [])
 
-    // }, [liState])
+    const handleWidth = e => {
+        setWidth(e.target.innerWidth)
+    }
+    // const [liState, setLiState] = useState({
+    //     "All": '',
+    //     'React.js': '',
+    //     'Node.js': '',
+    //     "Php": '',
+    // })
 
     const handleLiClick = e => {
-        // console.log(e.target.textContent)
-        // setLiState({
-        //     ...liState,
-        //     [e.target.textContent]: e.target.classList.toggle("background")
-        // })
-        // if (liState[e.target.textContent] === 'background'){
-
-        // }
     }
     return (
         <div>
             <main className="projects__main">
-                <div className="">
-                    <div className="project-languages__heading">
+                <div className="projects">
+                    <div className="project__language-headings">
                         <ul>
-                            <li onClick={handleLiClick}>All</li>
-                            <li onClick={handleLiClick}>React.js</li>
-                            <li onClick={handleLiClick}>Node.js</li>
-                            <li onClick={handleLiClick}>Php</li>
+                            <li onClick={handleLiClick}><strong>All</strong></li>
+                            <li onClick={handleLiClick}><strong>React.js</strong></li>
+                            <li onClick={handleLiClick}><strong>Node.js</strong></li>
+                            <li onClick={handleLiClick}><strong>Php</strong></li>
                         </ul>
+                    </div>
+                    <div className="project-cards">
+                        <div className="project-cards__image-holder">
+                            {width >= '720' ?
+                                <img src={instock} alt="instock project" />
+                                : <img src={instockMobile} alt="instock project" />}
+                        </div>
                     </div>
                 </div>
             </main>
