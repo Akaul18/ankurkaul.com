cbf839e79ec20e2f80d8a1eacaab9fbdc6acbec9
kaul <ankurkaul1894@gmail.com>
2020-04-29 22:52:05 -0700

    data successfully loading from api in projects component

--- src/components/Projects/ProjectCard/ProjectCards.js
+++ src/components/Projects/ProjectCard/ProjectCards.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import './ProjectCards.scss'
+import Button from '@material-ui/core/Button'
 
 const ProjectCards = ({ projects }) => {
     const [width, setWidth] = useState(0)
+
+    useEffect(() => {
+        // const imgCards = 
+    }, [])
+
     useEffect(() => {
         window.addEventListener('resize', handleWidth)
         return () => {
@@ -18,14 +24,29 @@ const ProjectCards = ({ projects }) => {
             <div className="project-cards__holder">
                 {projects.map(project =>
                     <div id={`img-${project.id}`} key={project.id} className="project-cards__image-holder">
-                        {/* <img src={`/assets/images/instock.png`} alt={project.title} /> */}
                         {width >= '720' ?
                             <img src={`/assets/images/${project.imgTitleDesktop}`} alt={project.title} />
                             : <img src={`/assets/images/${project.imgTitleDesktop}`} alt={project.title} />
                         }
+                        <div className="project-cards__proj-details">
+                            <div className="proj-details__title">{project.title}</div>
+                            {/* <div className="proj-details__desc">{project.desc}</div> */}
+                            <div className="proj-details__call-to-action">
+                                <Button className="call-to-action-buttons" variant="outlined" size="small" color="primary">
+                                    <strong>
+                                        <a href={project.github} taget="_blank">
+                                            View Github</a>
+                                    </strong>
+                                </Button>
+                                <Button className="call-to-action-buttons" variant="outlined" size="small" color="secondary">
+                                    <strong>
+                                        <a href={project.website} taget="_blank">Open Web App</a>
+                                    </strong>
+                                </Button>
+                            </div>
+                        </div>
                     </div>
                 )}
-                <div></div>
             </div>
         </div>
     );
