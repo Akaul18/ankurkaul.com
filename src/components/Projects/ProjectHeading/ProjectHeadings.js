bccd51ab7b4fd0b2af2bd9f064834c7ee6d3c192
kaul <ankurkaul1894@gmail.com>
2020-04-30 02:48:58 -0700

    added state for background change on click of headings in project component

--- src/components/Projects/ProjectHeading/ProjectHeadings.js
+++ src/components/Projects/ProjectHeading/ProjectHeadings.js
@@ -1,22 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import './ProjectHeadings.scss'
 
 function ProjectHeadings() {
 
+    const [state, setState] = useState({
+        "All": true,
+        "React.js": false,
+        "Node.js": false,
+        "Php": false,
+    })
+
     useEffect(() => {
+        console.log(state)
         const headingRef = document.getElementById('headingRef')
         headingRef.style.transform = 'translateY(0)'
-    }, [])
+    }, [state])
 
     const handleLiClick = e => {
+        // if (!state[e.target.textContent]) {
+        //     setState({ ...state, [e.target.textContent]: true })
+        //     e.target.style.background = "red"
+        // } else {
+        //     // setState({ ...state, [e.target.textContent]: false })
+        //     // e.target.style.background = "initial"
+        // }
     }
     return (
         <div id="headingRef" className="project__language-headings">
             <ul>
-                <li onClick={handleLiClick}><strong>All</strong></li>
-                <li onClick={handleLiClick}><strong>React.js</strong></li>
-                <li onClick={handleLiClick}><strong>Node.js</strong></li>
-                <li onClick={handleLiClick}><strong>Php</strong></li>
+                <li onClick={handleLiClick}>All</li>
+                <li onClick={handleLiClick}>React.js</li>
+                <li onClick={handleLiClick}>Node.js</li>
+                <li onClick={handleLiClick}>Php</li>
             </ul>
         </div>
     )
