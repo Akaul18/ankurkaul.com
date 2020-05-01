f8ce7fdfdc03ada2a910649c192119308d741c83
kaul <ankurkaul1894@gmail.com>
2020-04-30 20:58:27 -0700

    changing heading background on click in porject heading component

--- src/components/Projects/ProjectHeading/ProjectHeadings.js
+++ src/components/Projects/ProjectHeading/ProjectHeadings.js
@@ -3,35 +3,36 @@ import './ProjectHeadings.scss'
 
 function ProjectHeadings() {
 
-    const [state, setState] = useState({
-        "All": true,
-        "React.js": false,
-        "Node.js": false,
-        "Php": false,
-    })
+    // const [state, setState] = useState({
+    //     all: true,
+    //     react: false,
+    //     node: false,
+    //     php: false,
+    // })
 
     useEffect(() => {
-        console.log(state)
         const headingRef = document.getElementById('headingRef')
         headingRef.style.transform = 'translateY(0)'
-    }, [state])
+    }, [])
 
     const handleLiClick = e => {
-        // if (!state[e.target.textContent]) {
-        //     setState({ ...state, [e.target.textContent]: true })
-        //     e.target.style.background = "red"
-        // } else {
-        //     // setState({ ...state, [e.target.textContent]: false })
-        //     // e.target.style.background = "initial"
-        // }
+        // console.log(e.target.parentNode)
+        const ul = [...[e.target.parentNode.childNodes]]
+        ul[0].forEach(li => {
+            console.log(li)
+            li.classList.remove('background');
+            li.classList.add('background-negate')
+        })
+        e.target.classList.remove('background-negate')
+        e.target.classList.add('background')
     }
     return (
         <div id="headingRef" className="project__language-headings">
             <ul>
-                <li onClick={handleLiClick}>All</li>
-                <li onClick={handleLiClick}>React.js</li>
-                <li onClick={handleLiClick}>Node.js</li>
-                <li onClick={handleLiClick}>Php</li>
+                <li className="back background" onClick={handleLiClick}>All</li>
+                <li className="back" onClick={handleLiClick}>React.js</li>
+                <li className="back" onClick={handleLiClick}>Node.js</li>
+                <li className="back" onClick={handleLiClick}>Php</li>
             </ul>
         </div>
     )
