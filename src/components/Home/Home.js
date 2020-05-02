9ab10dd28bc8a06ae98bfc0d705f2d82e0964b9a
kaul <ankurkaul1894@gmail.com>
2020-05-02 12:19:30 -0700

    default loader removed from react-image-appear in home and projectCard

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import HireMe from '../HireMe'
 import { Link } from 'react-router-dom'
 import ReactImageAppear from 'react-image-appear'
-import BarLoader from 'react-spinners/BarLoader'
+// import BarLoader from 'react-spinners/BarLoader'
 
 const useStyles = makeStyles(theme => ({
     margin: {
@@ -15,19 +15,22 @@ const useStyles = makeStyles(theme => ({
 
 
 const Home = () => {
+
+    const [openDialog, setOpenDialog] = useState(false);
+    const [imgLoading, setImgLoading] = useState(true)
     React.useEffect(() => {
         const h1Ref = document.getElementById('h1Ref')
         const h3Ref = document.getElementById('h3Ref')
         const pRef = document.getElementById('pRef')
-        const imgRef = document.getElementById('imgRef')
+        // const imgRef = document.getElementById('imgRef')
+        // setImgLoading(document.getElementById('imgRef').complete)
 
         h1Ref.style.transform = 'translateX(0)';
         h3Ref.style.transform = 'translateY(0)';
         pRef.style.transform = 'translateY(0)';
-        imgRef.style.transform = 'scale(1)';
+        // imgRef.style.transform = 'scale(1)';
     }, [])
 
-    const [openDialog, setOpenDialog] = useState(false);
 
     const handleClickOpen = () => {
         setOpenDialog(true);
@@ -37,6 +40,11 @@ const Home = () => {
         setOpenDialog(false);
     };
 
+    const handleImgLoad = () => {
+        setImgLoading(false)
+        // const imgRef = document.getElementById('imgRef')
+        // setImgLoading(prevState => !prevState)
+    }
     const classes = useStyles();
     return (
         <div>
@@ -50,14 +58,16 @@ const Home = () => {
                             <h3 id="h3Ref">A Full stack developer <span><em>by choice </em> </span></h3>
                         </div>
                         <div className="self-intro__image">
-                            {/* <ReactImageAppear
+                            <ReactImageAppear
                                 src={`/assets/images/banner-img.jpg`}
                                 animation="bounceIn"
-                                easing="ease-in-out"
                                 animationDuration="1.5s"
-                                loader={<BarLoader loading />}
-                            /> */}
-                            <img id="imgRef" src={`/assets/images/banner-img.jpg`} alt="banner" />
+                                loader="none"
+                            />
+                            {/* <img id="imgRef"
+                                src={`/assets/images/banner-img.jpg`}
+                                alt="banner"
+                                onLoad={handleImgLoad} /> */}
                         </div>
                         <p id="pRef">I strongly <em>believe</em> that we can build our future as we want it to be. So, other than my interest in learning new Technologies and building Web based applications for almost 2 years now, I also like to keep working on myself to be a more <em>positive</em> and <em>self motivated person</em>.</p>
                         <div>
