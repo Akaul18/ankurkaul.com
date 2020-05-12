fb9adf6a468afe779ab2f30a38a054ad0623b020
kaul <ankurkaul1894@gmail.com>
2020-05-12 14:40:00 -0700

    made small changes like margin in contact smooth scroll timing in header and side drawer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,18 +16,18 @@ const Header = ({ drawerToggleHandler }) => {
                 {/* <div className="header__logo"><Link to="/"><strong>My Logo</strong></Link></div> */}
                 <div className="header__logo">
                     {/* <Link to="/"> */}
-                    <Link to="Home">
+                    <Link to="Home" spy={true} smooth={true} offset={-50} duration={500} delay={100}>
                         <img src="/assets/images/logo.png" alt="logo" />
                     </Link>
                 </div>
                 <div className="spacer"></div>
                 <div className="header__nav-items">
                     <ul>
-                        <li><Link to="Home" spy={true} smooth={true} offset={-50} duration={500} delay={100}><strong>HOME</strong></Link></li>
-                        <li><Link to="Skills" spy={true} smooth={true} offset={-50} duration={500} delay={100} ><strong>SKILLS</strong></Link></li>
-                        <li><Link to="Projects" spy={true} smooth={true} offset={-50} duration={500} delay={100} ><strong>PROJECTS</strong></Link></li>
+                        <li><Link to="Home" spy={true} smooth={true} offset={-50} duration={700} delay={100}><strong>HOME</strong></Link></li>
+                        <li><Link to="Skills" spy={true} smooth={true} offset={-50} duration={700} delay={100} ><strong>SKILLS</strong></Link></li>
+                        <li><Link to="Projects" spy={true} smooth={true} offset={-75} duration={700} delay={100} ><strong>PROJECTS</strong></Link></li>
                         {/* <li><Link to="/blog" ><strong>BLOG</strong></Link></li> */}
-                        <li><Link to="Contact" spy={true} smooth={true} offset={50} duration={500} delay={100} ><strong>CONTACT</strong></Link></li>
+                        <li><Link to="Contact" spy={true} smooth={true} offset={50} duration={700} delay={100} ><strong>CONTACT</strong></Link></li>
                     </ul>
                 </div>
             </nav>
--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -24,11 +24,11 @@ const SideDrawer = ({ showDrawer, closeDrawer }) => {
                 <li onClick={closeDrawer}><Link to="/projects"><strong>PROJECTS</strong></Link></li>
                 <li onClick={closeDrawer}><Link to="/blog"><strong>BLOG</strong></Link></li>
                 <li onClick={closeDrawer}><Link to="/contact"><strong>CONTACT</strong></Link></li> */}
-                <li><Link onClick={closeDrawer} to="Home" spy={true} smooth={true} offset={-50} duration={500} delay={100}><strong>HOME</strong></Link></li>
-                <li><Link to="Skills" onClick={closeDrawer} spy={true} smooth={true} offset={-50} duration={500} delay={100}><strong>SKILLS</strong></Link></li>
-                <li><Link to="Projects" onClick={closeDrawer} spy={true} smooth={true} offset={-75} duration={500} delay={100}><strong>PROJECTS</strong></Link></li>
+                <li><Link onClick={closeDrawer} to="Home" spy={true} smooth={true} offset={-50} duration={1000} delay={100}><strong>HOME</strong></Link></li>
+                <li><Link to="Skills" onClick={closeDrawer} spy={true} smooth={true} offset={-50} duration={1000} delay={100}><strong>SKILLS</strong></Link></li>
+                <li><Link to="Projects" onClick={closeDrawer} spy={true} smooth={true} offset={-75} duration={1000} delay={100}><strong>PROJECTS</strong></Link></li>
                 {/* <li><Link to="/blog" onClick={closeDrawer} spy={true} smooth={true} offset={-50} duration={500} delay={100}><strong>BLOG</strong></Link></li> */}
-                <li><Link to="Contact" onClick={closeDrawer} spy={true} smooth={true} offset={350} duration={500} delay={100}><strong>CONTACT</strong></Link></li>
+                <li><Link to="Contact" onClick={closeDrawer} spy={true} smooth={true} offset={370} duration={1000} delay={100}><strong>CONTACT</strong></Link></li>
             </ul>
         </nav>
     );
